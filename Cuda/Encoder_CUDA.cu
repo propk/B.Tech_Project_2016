e45b30a29dca12a633a42a85b2aaaf7aa95e93c8
Divyanshu Sharma <divyanshu.sharma.ece12@itbhu.ac.in>
2016-04-01 23:19:35 +0530

    Update Encoder_CUDA.cu

--- Cuda/Encoder_CUDA.cu
+++ Cuda/Encoder_CUDA.cu
@@ -530,11 +530,11 @@ int main()
     // second stage frequency transform
     EncSecondStageOverlapFilter<<< DimGrid2, 1>>>(imageDevice, imageHeight, imageWidth);
     // first stage pre-filtering
-    //EncFirstStagePreFiltering<<< DimGrid, DimBlock>>>(imageDevice, imageHeight, imageWidth);
+    EncFirstStagePreFiltering<<< DimGrid, DimBlock>>>(imageDevice, imageHeight, imageWidth);
     // first stage frequency transform
-    //EncFirstStageOverlapFilter<<< DimGrid2, 1>>>(imageDevice, imageHeight, imageWidth);
+    EncFirstStageOverlapFilter<<< DimGrid2, 1>>>(imageDevice, imageHeight, imageWidth);
     // second stage pre-filtering
-    //EncSecondStagePreFiltering<<< DimGrid, 1>>>(imageDevice, imageHeight, imageWidth);
+    EncSecondStagePreFiltering<<< DimGrid, 1>>>(imageDevice, imageHeight, imageWidth);
 
     /* kernel function invocation end*/
     cudaDeviceSynchronize();
